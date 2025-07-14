6832b48d7b4c5e41d8fd00e9422bcc0c143725eb
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-07-14 12:49:18 -0400

    Transition between register and login views is now operational

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -7,16 +7,17 @@ import { useRef } from 'react'
 
 const Login = () => {
     const registerRef = useRef<HTMLDivElement>(null);
+    const loginRef = useRef<HTMLDivElement>(null);
     
     return(
         <div className="w-full h-[100vh] flex flex-col justify-center items-center overflow-hidden">
             <BackgroundBlobs />
             <h1 className='text-4xl z-5 mb-10 font-bold max-[480px]:text-3xl'>Welcome to <span className='text-[#5D49EE]'>Invenfi</span></h1>
-            <div className='z-5 flex flex-col gap-5'>
+            <div className='z-5 flex flex-col gap-5' ref={loginRef}>
                 <LoginContainer />
                 <RegisterSection registerRef={registerRef}/>
             </div>
-            <Register ref={registerRef}/>
+            <Register ref={registerRef} loginRef={loginRef}/>
         </div>
     )
 }
--- front-end/src/views/Register/Register.tsx
+++ front-end/src/views/Register/Register.tsx
@@ -1,4 +1,6 @@
 import { forwardRef, useRef } from "react";
+import type { RefObject } from "react";
+
 import { FormFieldSpec } from "./FormFieldSpec";
 import type { ValuesMap } from "../../components/Blocks/Form/FieldForm";
 import FieldForm from "../../components/Blocks/Form/FieldForm";
@@ -6,7 +8,11 @@ import TransparentContainer from "../../components/Blocks/TransparentContainer/T
 import { isValid } from "../../utils/IsValid";
 import Button from "../../components/Button";
 
-const Register = forwardRef<HTMLDivElement>((_, ref) => {
+type RegisterProps = {
+  loginRef: RefObject<HTMLDivElement | null>;
+};
+
+const Register = forwardRef<HTMLDivElement, RegisterProps>(({ loginRef }, ref) => {
   const fieldDataRef = useRef<ValuesMap>({});
   const formRef = useRef<HTMLDivElement>(null);
 
@@ -30,18 +36,42 @@ const Register = forwardRef<HTMLDivElement>((_, ref) => {
     }
   };
 
+  const goToLogin = () => {
+    const registerForm = (ref as RefObject<HTMLDivElement>)?.current;
+    const loginForm = loginRef.current;
+    console.log(registerForm)
+
+    if (registerForm && loginForm) {
+      registerForm.classList.remove("fade-in-right")
+      registerForm.classList.add("fade-out-left")
+      setTimeout(() => {
+        registerForm.classList.add("hidden")
+        loginForm.classList.remove("hidden")
+        loginForm.classList.remove("fade-out-left")
+        loginForm.classList.add("fade-in-right")
+      }, 300);
+    }
+  };
+
   return (
     <TransparentContainer ref={ref} formSize={2}>
       <h1 className="font-medium text-3xl mb-5">Register</h1>
+
       <FieldForm
         formSize={2}
         containerRef={formRef}
         inputParams={FormFieldSpec}
         sendInfo={retrieveFormData}
       />
+
       <div className="flex flex-row w-full gap-10">
-        <Button onClick={registerUser} bgColor="bg-gradient-to-r from-[#5E6AEE] to-[#CF8EEB]">Sign Up</Button>
-        <Button onClick={registerUser} color="#434343" thin>Already have an account? Login</Button>
+        <Button onClick={registerUser} bgColor="bg-gradient-to-r from-[#5E6AEE] to-[#CF8EEB]">
+          Sign Up
+        </Button>
+
+        <Button onClick={goToLogin} color="#434343" thin>
+          Already have an account? Login
+        </Button>
       </div>
     </TransparentContainer>
   );
